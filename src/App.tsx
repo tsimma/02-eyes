aa6be7ae7ea74e87be1a8b5e94cfbf95f459cef8
Alex Chuev <alex@chuev.com>
2025-01-02 14:14:04 +0300

    Add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { Eyes } from './Eyes'
 import { FC } from 'react'
+import { Container } from './Container'
 
 export const App: FC = () => {
-  return <Eyes />
+  return (
+    <Container>
+      <Eyes x={12} y={23} />
+    </Container>
+  )
 }
--- src/Eyes.tsx
+++ src/Eyes.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react'
 
-export const Eyes: FC = () => {
+export const Eyes: FC<{
+  x: number
+  y: number
+}> = ({ x, y }) => {
   return (
-    <div className="h-screen flex items-center justify-center contrast-125">
-      <div
-        className={`hover:bg-[url("./resources/closedeyes.jpg")] bg-[url("./resources/openeyes.jpg")] w-[500px] h-[200px] bg-cover bg-center bg-no-repeat`}
-      />
+    <div
+      className={`hover:bg-[url("./resources/closedeyes.jpg")] bg-[url("./resources/openeyes.jpg")] w-[500px] h-[200px] bg-cover bg-center bg-no-repeat  contrast-125`}
+    >
+      {x}x{y}
     </div>
   )
 }
