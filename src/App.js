78729d88291e5bdb117fc4cb090cc0c4c4680ab2
Alex Chuev <alex@chuev.com>
2025-01-02 13:06:44 +0300

    Replace class with className

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="app-wrapper" class="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <div
-        class="hover:bg-[url('https://avatars.mds.yandex.net/i?id=74b8201b275acef1758fc43b604d312d_l-5350805-images-thumbs&n=13')]
+        className="hover:bg-[url('https://avatars.mds.yandex.net/i?id=74b8201b275acef1758fc43b604d312d_l-5350805-images-thumbs&n=13')]
                         bg-[url('https://cdn131.picsart.com/284091599014211.png?type=webp&to=min&r=640')]
                         w-[500px] h-[200px] bg-cover bg-center bg-no-repeat "
       ></div>
