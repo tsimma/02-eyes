28956f31c9d03f18fa4e226e8789a575329f7b7f
Alex Chuev <alex@chuev.com>
2025-01-02 15:26:05 +0300

    Add eyes state factory implementation

--- src/EyesStateFactory.ts
+++ src/EyesStateFactory.ts
@@ -5,6 +5,38 @@ export class EyesStateFactory {
   constructor(private coordinates: Coordinates | null) {}
 
   getState(): EyesState {
-    return EyesState.Closed
+    if (this.coordinates === null) {
+      return EyesState.Direct
+    }
+
+    return this.getStateByCoordinates(this.coordinates)
+  }
+
+  private getStateByCoordinates([x, y]: Coordinates): EyesState {
+    if (x < 0.33) {
+      if (y < 0.33) {
+        return EyesState.TopLeft
+      } else if (y < 0.66) {
+        return EyesState.Left
+      } else {
+        return EyesState.BottomLeft
+      }
+    } else if (x < 0.66) {
+      if (y < 0.33) {
+        return EyesState.Top
+      } else if (y < 0.66) {
+        return EyesState.Closed
+      } else {
+        return EyesState.Bottom
+      }
+    } else {
+      if (y < 0.33) {
+        return EyesState.TopRight
+      } else if (y < 0.66) {
+        return EyesState.Right
+      } else {
+        return EyesState.BottomRight
+      }
+    }
   }
 }
