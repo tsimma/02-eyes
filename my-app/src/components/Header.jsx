0da35f00bd479ad471e5392ef191fe6aa2086b60
qwert <qwert@DESKTOP-ICRBVL2>
2025-01-02 12:27:57 +0300

    Добавьте файлы проекта.

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className='header'>
+        <img src='https://zip.re/wp-content/uploads/2021/12/dell-1.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return <div className='content'>
+        <div className='centralImage'>
+            <img src='https://artwall.kz/image/shutterDetailed/524664706.jpg' />
+        </div>
+        <div>
+            ava+description
+            <img src='https://gas-kvas.com/uploads/posts/2023-02/1675551256_gas-kvas-com-p-risunki-zhivotnie-kvadratnie-33.jpg' />
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>post1</div>
+                <div>post2</div>
+
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
