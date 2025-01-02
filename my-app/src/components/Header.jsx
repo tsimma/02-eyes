d2cd5932ef1b9c4f9468c1989b0498d608dc893c
Alex Chuev <alex@chuev.com>
2025-01-02 12:51:13 +0300

    Reorganized files

--- my-app/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-    return <header className='header'>
-        <img src='https://zip.re/wp-content/uploads/2021/12/dell-1.png'></img>
-    </header>
-}
-
-export default Header;
