ffd2f4eab0d089cbaed01c7a3fd407e9bead571a
Alex Chuev <alex@chuev.com>
2025-01-03 13:57:42 +0300

    Use Coordinates type in hook

--- src/hooks/useEyesState.ts
+++ src/hooks/useEyesState.ts
@@ -1,7 +1,8 @@
 import { useMemo } from 'react'
 import { EyesStateFactory } from '../EyesStateFactory'
 import { EyesState } from '../types/EyesState'
+import { Coordinates } from '../types/Coordinates'
 
-export function useEyesState(coordinates: [x: number, y: number] | null): EyesState {
+export function useEyesState(coordinates: Coordinates | null): EyesState {
   return useMemo(() => new EyesStateFactory(coordinates).getState(), [coordinates])
 }
